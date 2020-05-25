9ce9ecf50a6cec8399436c6907aa8523970a54c2
Seth Nielsen <sethmnielsen@gmail.com>
2020-05-25 15:07:41 -0600

    nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,6 +1,13 @@
 pub fn nth(n: u32) -> u32 {
+    // p will be the returned value
+    let mut p: u32 = 0;
     // lookup for primes < 10; 2 and 5 are notable exceptions for heuristics
     let primes: Vec<u32> = vec![2, 3, 5, 7]; // primes for n=1, 2, 3, or 4
+
+    // use lookup if n < 5
+    if n < 5 {
+        p = primes[n-1];
+    }
 
     // counter to keep track of how many primes have been found
     let mut k: u32 = 0;
