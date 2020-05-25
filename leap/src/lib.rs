b63e499b488b4315affdf5657dac0bf530b54754
Seth Nielsen <sethmnielsen@gmail.com>
2020-05-24 22:05:26 -0600

    Raindrops WIP

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,6 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if (year % 400  == 0) || (year %) {
-        return true
+    if year % 400  == 0 || (year % 4 == 0 && year % 100 != 0) {
+        true
     }
-    true
+    else {
+        false
+    }
 }
