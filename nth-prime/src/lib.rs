008d6a54eef070f33a171cea25bf9dd9ddca6878
Seth Nielsen <sethmnielsen@gmail.com>
2020-05-25 19:27:20 -0600

    nth prime shortcuts

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,18 +1,27 @@
 pub fn nth(n: u32) -> u32 {
+    let n: usize = n as usize;
     // p will be the returned value
     let mut p: u32 = 0;
     // lookup for primes < 10; 2 and 5 are notable exceptions for heuristics
-    let primes: Vec<u32> = vec![2, 3, 5, 7]; // primes for n=1, 2, 3, or 4
+    let mut primes: Vec<u32> = Vec::with_capacity(n);
+    primes.extend([2, 3, 5, 7].iter().copied());
 
     // use lookup if n < 5
     if n < 5 {
-        p = primes[n-1];
+        let nidx = n-1; // index must be usize
+        p = primes[nidx];
+    }
+    else {
+        // the value that will be incremented to test for prime-ness
+        let mut x: u32 = 7;
+
+        while primes.len() < n {
+            x += 2;
+            
+            // if x %
+        }
     }
 
-    // counter to keep track of how many primes have been found
-    let mut k: u32 = 0;
-    // the value that will be incremented to test for prime-ness
-    let mut x: u32 = 17;
-
-    while k < n
+    // return solution p
+    p
 }
